11c36efb9eb2b6e700f951b4d26dba3a077429c2
ihab abadi <ihab@utopios.net>
2024-10-16 11:33:14 +0200

    debut de correction portfolio

---
+++ demo_java_clean_code/demo_java_clean_code/src/main/java/org/example/jour3/correction_exercice_value_object/Portfolio.java
@@ -0,0 +1,17 @@
+package org.example.jour3.correction_exercice_value_object;
+
+import java.util.List;
+
+public class Portfolio {
+    private final List<Investment> investments;
+
+
+    public Portfolio(List<Investment> investments) {
+        this.investments = investments;
+    }
+
+
+    public void displayInvestments() {
+
+    }
+}
---
+++ demo_java_clean_code/demo_java_clean_code/src/main/java/org/example/jour3/correction_exercice_value_object/PortfolioDisplayer.java
@@ -0,0 +1,15 @@
+package org.example.jour3.correction_exercice_value_object;
+
+public class PortfolioDisplayer {
+
+    private final Portfolio portfolio;
+
+    public PortfolioDisplayer(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+
+    public void display() {
+
+    }
+}
