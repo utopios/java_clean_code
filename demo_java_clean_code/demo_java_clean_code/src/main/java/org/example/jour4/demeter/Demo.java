3c10ed8d8c27b3a911bdcebe9f9a16c288c75c5d
ihab abadi <ihab@utopios.net>
2024-11-21 16:23:26 +0100

    sujet video store

---
+++ demo_java_clean_code/demo_java_clean_code/src/main/java/org/example/jour4/demeter/Demo.java
@@ -0,0 +1,23 @@
+package org.example.jour4.demeter;
+
+public class Customer {
+    private Account account;
+
+    public double getBalance() {
+        return account.getBalance();
+    }
+}
+
+public class Order {
+    private Customer customer;
+
+    public double getCustomerBalance() {
+        return customer.getBalance();
+    }
+}
+
+public class OrderService {
+    public double getCustomerBalance(Order order) {
+        return order.getCustomerBalance();
+    }
+}
